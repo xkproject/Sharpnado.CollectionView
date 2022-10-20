802836acca36b98db46891012accc87b6d1c13ef
Edgar Martínez Serrano <edgarlives1999@gmail.com>
2022-10-20 10:40:38 +0200

    Implemented animated DataTemplateSelector

---
+++ Sharpnado.CollectionView/Helpers/AnimatedDataTemplateSelector.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sharpnado.CollectionView.Helpers
+{
+    public abstract class AnimatedDataTemplateSelector : DataTemplateSelector
+    {
+        public abstract Task AnimateSelectedDataTemplateAsync(object item, BindableObject container, ViewCell viewCell);
+    }
+}
